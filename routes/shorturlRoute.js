e9a09c1129dc112fe0c15cd69a6c582003820893
unknown <samad.saran@gmail.com>
2024-03-03 00:39:36 +0530

    routes modified

--- routes/shorturlRoute.js
+++ routes/shorturlRoute.js
@@ -6,7 +6,9 @@ router.post('/', async (req, res) => {
   try {
     await shortUrlModel.create({ full: req.body.fullUrl });
     res.redirect('/');
-  } catch (error) {}
+  } catch (error) {
+    res.status(500).send('internal server error');
+  }
 });
 
 module.exports = router;
